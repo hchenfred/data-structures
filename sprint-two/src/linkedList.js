480a41aae0ab0e36906628c6bea56f05cc149ccd
hchenfred <hchen.fred@gmail.com>
2017-02-08 16:15:28 -0800

    Complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      var listHead = list.head.value;
+      list.head = list.head.next;
+      return listHead;
+    }
+    return undefined;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false; 
   };
-
   return list;
 };
 
