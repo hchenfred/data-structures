472215bd35bd906df5263362ad08dcb3e3c353b6
hchenfred <hchen.fred@gmail.com>
2017-02-10 11:00:00 -0800

    Completed hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,21 +1,35 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  for (var i = 0; i < this._limit; i++) {
+    this._storage.set(i, []);
+  }
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  this._storage.get(index).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var value;
+  _.each(this._storage.get(index), function(tuple) {
+    if (k === tuple[0]) {
+      value = tuple[1];
+    }
+  });
+  return value;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
+  var that = this;
+  _.each(this._storage.get(index), function(tuple, i) {
+    if (k === tuple[0]) {
+      that._storage.get(index).splice(i, 1);
+    }
+  });
 };
 
 
